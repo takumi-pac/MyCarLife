879474562d7778d59511c50123deaa14cb2e57a9
takumi-pac <iostakumi199@gmail.com>
2023-01-24 15:00:05 +0900

    Initial Commit

---
+++ MyCarLife/MyCarLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarLifeApp.swift
+//  MyCarLife
+//
+//  Created by 松田拓海 on 2023/01/24.
+//
+
+import SwiftUI
+
+@main
+struct MyCarLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
